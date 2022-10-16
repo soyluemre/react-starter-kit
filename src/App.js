74934bfe314c1f26f8be4482a3d091a6d0f20d70
soyluemre <emresoyludev@gmail.com>
2022-10-16 20:47:55 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import logo from "./logo.svg";
+import "./style.css";
+import Bootstrap from "./Bootstrap";
+
 function App() {
-  return <div className="App">TEST</div>;
+  return (
+    <div className="App">
+      <Bootstrap />
+      <img src={logo} alt="" />
+    </div>
+  );
 }
 
 export default App;
