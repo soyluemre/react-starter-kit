c59650f7646dc6d4e386d2def2962069a87ac6f2
soyluemre <emresoyludev@gmail.com>
2022-10-16 20:46:26 +0300

    start bootstrap and components

---
+++ src/Bootstrap.js
@@ -0,0 +1,13 @@
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Bootstrap() {
+  return (
+    <div>
+      <h3>Bootstrap</h3>
+      <Button className="btn btn-warning w-25 p-3 fw-bold fs-2">Button</Button>
+    </div>
+  );
+}
+
+export default Bootstrap;
